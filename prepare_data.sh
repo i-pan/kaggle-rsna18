7c52dbc401c2ca60cab04d622faa21bb8bfd7825
Ian Pan <i-pan@users.noreply.github.com>
2019-03-10 10:24:32 -0400

    fix COCOify statement

--- prepare_data.sh
+++ prepare_data.sh
@@ -9,7 +9,8 @@ python src/etl/4_CreateResizedNumpyArrays.py
 echo "Creating flipped test images for later inference ..."
 python src/etl/5_FlipTestImages.py
 echo "Transforming data into COCO format ..."
-python src/etl/6_COCOify.py
+python src/etl/6_COCOify.py train
+python src/etl/6_COCOify.py test
 echo "Generating concatenated images for ConcatRetinaNet ..."
 python src/etl/7_CreateConcatImages.py 
 echo "Generating annotations for training keras-retinanet ..."
